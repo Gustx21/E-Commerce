5af79e59d1fb8b9e4808ef09bd5aa0218fca73c6
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-05-19 08:29:56 -0300

    Adicionando o try catch
    Lidar com erros da requisição com try catch.

--- terciario/js/script.js
+++ terciario/js/script.js
@@ -4,17 +4,22 @@ import { error } from "../../error/script.js";
 document.getElementById("enviar").addEventListener("click", inserirUser);
 
 async function inserirUser() {
-    const email = document.getElementById("email").value;
-    const nome = document.getElementById("nome").value;
-    const texto = document.getElementById("comentario").value;
-
-    let response = await fetch("http://127.0.0.1:8080/user",
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email, nome, texto })
-        }
-    )
-
-    error(response);
+    try {
+        const email = document.getElementById("email").value;
+        const nome = document.getElementById("nome").value;
+        const texto = document.getElementById("comentario").value;
+    
+        let response = await fetch("http://127.0.0.1:8080/user",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, nome, texto })
+            }
+        )
+        
+        error(response);
+        
+    } catch (error) {
+        error(error);
+    }
 }
